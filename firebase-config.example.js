a0cb5d64b9ac40e98fa7ca8dceefc1731565fcc4
Sophia Niezabitowska <s.niezabitowska@outlook.com>
2026-04-29 21:18:18 +0100

    Create firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// firebase-config.example.js
+// Replace these with your own Firebase project credentials
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID"
+};
